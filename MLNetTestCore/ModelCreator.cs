2df1ec2a2080e45c3c7eb2601164602bc5e36353
Joe <joe.andrew.bennett@gmail.com>
2022-01-13 07:23:12 -0500

    Add project files.

---
+++ MLNetTestCore/ModelCreator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+
+namespace MLNetTestCore
+{ 
+    public static class ModelCreator
+    {
+        public static void Run(String imagePath)
+        {
+            ClassifyCatOrDog(imagePath);
+        }
+
+        public static void ClassifyCatOrDog(String imagePath)
+        {
+            //Load sample data
+            var imageBytes = File.ReadAllBytes(imagePath);
+            Cat_dog.ModelInput sampleData = new Cat_dog.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            //Load model and predict output
+            var result = Cat_dog.Predict(sampleData);
+            Console.WriteLine($"Image: {Path.GetFileName(imagePath)} predicted as: {result.PredictedLabel} with score: {result.Score.Max()} ");
+
+        }
+    }
+}
---
+++ MLNetTrainer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MLNetTestCore; 
+
+namespace MLNetTrainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLNetTestCore.ModelCreator.Run(args[0]);
+        }
+    }
+}
